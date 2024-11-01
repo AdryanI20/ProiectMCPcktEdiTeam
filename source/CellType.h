e26097204305aa4105fe901fca35f2b8ce4b7d9e
Norris097 <157019021+Norris097@users.noreply.github.com>
2024-11-01 12:38:11 +0200

    Cell

---
+++ source/CellType.h
@@ -0,0 +1,16 @@
+//
+// Created by norri on 10/30/2024.
+//
+
+#ifndef PROIECTMC_CELLTYPE_H
+#define PROIECTMC_CELLTYPE_H
+
+
+enum CellType {
+    FREE_SPACE,
+    DESTRUCTIBIL_WALL,
+    INDESTRUCTIBIL_WALL
+};
+
+
+#endif //PROIECTMC_CELLTYPE_H
