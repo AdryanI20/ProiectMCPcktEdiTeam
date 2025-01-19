859e6c0073c699db30a2f400c63ab3941c1e1b67
Norris097 <157019021+Norris097@users.noreply.github.com>
2025-01-19 19:45:14 +0200

    SB 1/2

---
+++ ServerMC/source/SpecialBulletItem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by norri on 1/19/2025.
+//
+
+#include "SpecialBulletItem.h"
---
+++ ServerMC/source/SpecialBulletItem.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "GameObject.h"
+#include "PlayerObject.h"
+#include "Map.h"
+#include "CellType.h"
+#include <map>
+#include <memory>
+
+
+class SpecialBulletItem : public GameObject {
+public:
+
+    SpecialBulletItem(int x, int y, const std::string& textureID = "special_bullet_item");
+    
+    void Update(Map& map, std::map<std::string, std::shared_ptr<GameObject>>& gameObjects);
+
+    virtual std::string getID() override;
+
+    bool m_destroy;
+};
